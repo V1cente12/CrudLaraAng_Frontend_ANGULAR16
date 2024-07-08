97b00b8d2a0e4a107bfdec1cc3b856e71a5dce0c
V1cente12 <jvicentecv@gmail.com>
2024-07-08 18:08:11 -0400

    servicio categoria

---
+++ src/app/categoria.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+  private apiUrl = 'http://127.0.0.1:8000/api';
+  constructor(private http: HttpClient) { }
+
+  getCategorias(): Observable<any>{
+    return this.http.get<any>('${this.apiUrl}/categoria');
+  }
+
+  getCategoriaById(id:number): Observable<any>{
+    return this.http.get<any>('${this.apiUrl}/categoriaid/${id}');
+  }
+
+  AddCategoria(categoria:any): Observable<any>{
+    return this.http.post<any>('${this.apiUrl}/addCategoria', categoria);
+  }
+
+  updateCategoria(id: number, categoria: any): Observable<any>{
+    return this.http.put<any>('${this.apiUrl}/updateCategoria/${id}', categoria);
+  }
+
+  deleteCategoria(id:number): Observable<any>{
+    return this.http.delete<any>('${this.apiUrl}/deleteCategoriabyid/${id}');
+  }
+}
