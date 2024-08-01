fe913753a6963b6a3d8e14f3d0749db5404d3038
Vicente Calderon <77591317+V1cente12@users.noreply.github.com>
2024-07-31 23:58:33 -0400

    Actualizar categoria.service.ts

--- src/app/categoria.service.ts
+++ src/app/categoria.service.ts
@@ -10,26 +10,32 @@ export class CategoriaService {
   private apiUrl = 'http://127.0.0.1:8000/api';
   constructor(private http: HttpClient) { }
 
+  //obtener categorias
   getCategorias(): Observable<any>{
     return this.http.get<any>(`${this.apiUrl}/categoria`);
   }
 
+  //obtener categoria por id
   getCategoriaById(id:number): Observable<any>{
     return this.http.get<any>(`${this.apiUrl}/categoriaid/${id}`);
   }
 
+  //obtener categoria por nom
   getCategoriabyCat_nom(cat_nom:string): Observable<any>{
     return this.http.get<any>(`${this.apiUrl}/categoriacat_nom/${cat_nom}`);
   }
 
+  //añadir categoria
   AddCategoria(categoria:any): Observable<any>{
     return this.http.post<any>(`${this.apiUrl}/addCategoria`, categoria);
   }
 
+  //actualizar categoria
   updateCategoria(id: number, categoria: any): Observable<any>{
     return this.http.put<any>(`${this.apiUrl}/updateCategoria/${id}`, categoria);
   }
 
+  //eliminar categoria
   deleteCategoria(id:number): Observable<any>{
     return this.http.delete<any>(`${this.apiUrl}/deleteCategoriabyid/${id}`);
   }
